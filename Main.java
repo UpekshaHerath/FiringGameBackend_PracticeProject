c80ff4eb6d4e318eadee2c08e955d7739d10161b
UpekshaHerath <65499669+UpekshaHerath@users.noreply.github.com>
2021-08-02 21:18:45 -0700

    Update Main.java
    removed some comments

--- Main.java
+++ Main.java
@@ -4,13 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         PlayerOne player = new PlayerOne("Upeksha", 5000, "AK47");
-//        player.setHealth(500);
-//        System.out.println(player.getHealth());
-//        player.hurtByGunOne();
-//        player.hurtByGunOne();
-//        player.hurtByGunOne();
-//        player.hurtByGunOne();
-//        System.out.println(player.getHealth());
 
         PlayerTwo playertwo = new PlayerTwo("Dilshan", 100, "rifal", true);
         playertwo.hurtByGunOne();
